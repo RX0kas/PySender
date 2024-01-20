d6905cedd343b4ca9bbdadcbf0a19b4f7a7c4b60
ninjagoku4560 <nathangabard@outlook.fr>
2024-01-20 21:30:45 +0100

    Debug init.py

--- PySender/PySender.py
+++ PySender/PySender.py
@@ -1,20 +1,39 @@
 import os
+from tkinter import ttk,Tk
+from logger import Logger
+import sys
 
-import tkinter as tk
-from logger import logger
 
-LOGGER = logger(True)
 
-LOGGER.log("MIT License")
-LOGGER.log("Copyright (c) 2024 ninjagoku4560")
+LOGGER = Logger(True)
 
-if not os.path.exists("Data"):
-    LOGGER.log("First launch detected starting init script")
+
+if not os.path.exists("parameter.txt"):
+    LOGGER.warning("First launch detected starting init script")
+    
+    # Temporary
+    with open("parameter.txt","w") as file:
+        file.write("")
+    # Temporary
+
     os.system("python init.py")
     os.system("md Data")
 
-class app(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.pack()
+
+def start():
+    LOGGER.log("MIT License")
+    LOGGER.log("Copyright (c) 2024 ninjagoku4560")
+    root = Tk()
+    root.title("PySender v0.0.1")
+
+    frm = ttk.Frame(root, padding=10)
+    frm.grid()
+    ttk.Label(frm, text="PySender").grid(column=0, row=0)
+    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+    root.mainloop()
+
+
+
+if __name__ == "__main__":
+    start()
  
--- PySender/init.py
+++ PySender/init.py
@@ -1,23 +1,44 @@
+# coding: utf-8
 from os import system as execute
+from os import path,makedirs
+from PySender import LOGGER
 
-print("\033[34mInitiation of the installation of the lib")
-print("Updating pip...\033[0m")
-execute("python -m pip install --upgrade pip")
 
-print("\033[34mInstallation of pyClamd \033[0m")
-execute("pip install pyClamd")
-execute("pip install -U pyClamd")
-print("\033[92mInstallation Done\033[0m")
+def create_DIR(dir_name):
+    if not path.exists(dir_name):
+        makedirs(dir_name)
+        LOGGER.log(f"The folder '{dir_name}' was create")
 
-print("\033[34mInstallation of tkinter \033[0m")
-execute("pip install tk") 
-execute("pip install -U tk")
-print("\033[92mInstallation Done\033[0m")
-
-print("\033[92mAll Installation Done")
-print("\033[34mStarting the parameter creation script...")
+LOGGER.log("Starting the parameter creation script...")
 from parameter import first_start
 first_start()
 
-print("Starting for the first time...\033[0m")
-execute("python PySender.py")
+
+LOGGER.log("Initiation of the installation of the lib")
+LOGGER.log("Updating pip...")
+execute("python -m pip install --upgrade pip")
+
+LOGGER.log("Installation of pyClamd")
+execute("pip install pyClamd")
+execute("pip install -U pyClamd")
+LOGGER.log("Installation Done")
+
+LOGGER.log("Installation of tkinter")
+execute("pip install tk") 
+execute("pip install -U tk")
+LOGGER.log("Installation Done")
+
+LOGGER.log("All Installation Done")
+
+
+
+
+LOGGER.log("Starting the folder creation script...")
+create_DIR("LOG")
+LOGGER.log("Creation Done")
+
+
+
+
+LOGGER.log("Starting for the first time...")
+execute("python PySender.py True")
